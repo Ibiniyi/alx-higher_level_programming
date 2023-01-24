f47ee1c0b8b34065fc6c6990a1da4d04a1c25898
Ibiniyi <Timothyniyi@gmail.com>
2023-01-23 20:04:40 -0800

    pyhton exception

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
+    else:
+        return True
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
